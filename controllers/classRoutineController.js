dc32bf225ed21af5407b7ee0c310dc5df950e98a
tauhid97k <tauhid.coder@gmail.com>
2024-02-28 13:22:30 +0600

    class routine api

--- controllers/classRoutineController.js
+++ controllers/classRoutineController.js
@@ -9,9 +9,10 @@ const {
   examValidator,
   examStatusUpdateValidator,
 } = require('../validators/examValidator')
+const { classRoutineValidator } = require('../validators/classRoutineValidator')
 
 /*
-  @route    GET: /exams
+  @route    GET: /class-routines
   @access   private
   @desc     All exams
 */
@@ -75,83 +76,48 @@ const getAllClassRoutines = asyncHandler(async (req, res, next) => {
 })
 
 /*
-  @route    GET: /exams/:id
+  @route    GET: /class-routines/:id
   @access   private
-  @desc     Get an exam details
+  @desc     Get class routine details
 */
-const getExam = asyncHandler(async (req, res, next) => {
+const getClassRoutine = asyncHandler(async (req, res, next) => {
   const id = Number(req.params.id)
   await prisma.$transaction(async (tx) => {
-    const findExam = await tx.exams.findUnique({
+    const classRoutine = await tx.class_routines.findUnique({
       where: {
         id,
       },
-      include: {
-        exam_category: true,
-        exam_classes: {
-          include: {
-            class: true,
+      select: {
+        id: true,
+        week_day: true,
+        created_at: true,
+        updated_at: true,
+        class: {
+          select: {
+            class_name: true,
           },
         },
-        exam_sections: {
-          include: {
-            section: true,
-          },
-        },
-        exam_routines: {
-          include: {
-            subject: true,
-          },
-          orderBy: {
-            start_time: 'asc',
+        section: {
+          select: {
+            section_name: true,
           },
         },
       },
     })
 
-    if (!findExam)
+    if (!classRoutine)
       return res.status(404).json({
-        message: 'No exam found',
+        message: 'No routine found',
       })
 
     // Format Data
     const formatData = {
-      id: findExam.id,
-      status: findExam.status,
-      exam_date: findExam.exam_routines.at(0).start_time,
-      exam_category: findExam.exam_category,
-      classes: findExam.exam_classes.map(({ class: { id, class_name } }) => ({
-        id,
-        class_name,
-      })),
-      sections: findExam.exam_sections.map(
-        ({ section: { id, class_id, section_name } }) => ({
-          id,
-          class_id,
-          section_name,
-        })
-      ),
-      exam_routine: findExam.exam_routines.map(
-        ({
-          id,
-          full_mark,
-          start_time,
-          end_time,
-          subject: { id: subjectId, name, code },
-        }) => ({
-          id,
-          full_mark,
-          start_time,
-          end_time,
-          subject: {
-            id: subjectId,
-            name,
-            code,
-          },
-        })
-      ),
-      created_at: findExam.created_at,
-      updated_at: findExam.updated_at,
+      id: classRoutine.id,
+      week_day: classRoutine.week_day,
+      class_name: classRoutine.class.class_name,
+      section_name: classRoutine.section.section_name,
+      created_at: classRoutine.created_at,
+      updated_at: classRoutine.updated_at,
     }
 
     res.json(formatData)
@@ -159,208 +125,89 @@ const getExam = asyncHandler(async (req, res, next) => {
 })
 
 /*
-  @route    POST: /exams
+  @route    POST: /class-routine
   @access   private
-  @desc     Create a new exam
+  @desc     Create new class routine
 */
-const createExam = asyncHandler(async (req, res, next) => {
-  const { exam_category_id, classes, sections, exam_routine } =
-    await examValidator().validate(req.body, {
-      abortEarly: false,
-    })
-
-  // Format Data For Database
-  const formatClasses = classes.map((class_id) => ({ class_id }))
-  const formatSections = sections.map((section_id) => ({ section_id }))
-
-  await prisma.$transaction(async (tx) => {
-    const exam = await tx.exams.create({
-      data: {
-        exam_category_id,
-        exam_classes: {
-          createMany: {
-            data: formatClasses,
-          },
-        },
-        ...(formatSections.length > 0 && {
-          exam_sections: {
-            createMany: {
-              data: formatSections,
-            },
-          },
-        }),
-        exam_routines: {
-          createMany: {
-            data: exam_routine,
-          },
-        },
-      },
-    })
-
-    // Create result publish
-    await tx.exam_results_publish.create({
-      data: {
-        exam_id: exam.id,
-      },
-    })
+const createClassRoutine = asyncHandler(async (req, res, next) => {
+  const data = await classRoutineValidator().validate(req.body, {
+    abortEarly: false,
   })
 
-  res.status(201).json({ message: 'Exam created successfully' })
+  await prisma.class_routines.create({
+    data,
+  })
+
+  res.status(201).json({ message: 'Class routine created' })
 })
 
 /*
-  @route    PUT: /exams/:id
+  @route    PUT: /class-routines/:id
   @access   private
-  @desc     Update an exam
+  @desc     Update class routine
 */
-const updateExam = asyncHandler(async (req, res, next) => {
+const updateClassRoutine = asyncHandler(async (req, res, next) => {
   const id = Number(req.params.id)
 
-  const { exam_category_id, classes, sections, exam_routine } =
-    await examValidator().validate(req.body, {
-      abortEarly: false,
-    })
-
-  await prisma.$transaction(async (tx) => {
-    const findExam = await tx.exams.findUnique({
-      where: {
-        id,
-      },
-    })
-
-    if (!findExam)
-      return res.status(404).json({
-        message: 'No exam found',
-      })
-
-    // Delete existing entries in exam classes
-    await tx.exam_classes.deleteMany({
-      where: {
-        exam_id: findExam.id,
-      },
-    })
-
-    // Delete existing entries in exam sections
-    await tx.exam_sections.deleteMany({
-      where: {
-        exam_id: findExam.id,
-      },
-    })
-
-    // Delete existing entries in exam routine
-    await tx.exam_routines.deleteMany({
-      where: {
-        exam_id: findExam.id,
-      },
-    })
-
-    // Format Data For Database
-    const formatClasses = classes.map((class_id) => ({ class_id }))
-    const formatSections = sections.map((section_id) => ({ section_id }))
-
-    await tx.exams.update({
-      where: {
-        id: findExam.id,
-      },
-      data: {
-        exam_category_id,
-        exam_classes: {
-          createMany: {
-            data: formatClasses,
-          },
-        },
-        ...(formatSections.length > 0 && {
-          exam_sections: {
-            createMany: {
-              data: formatSections,
-            },
-          },
-        }),
-        exam_routines: {
-          createMany: {
-            data: exam_routine,
-          },
-        },
-      },
-    })
-
-    res.json({ message: 'Exam updated successfully' })
-  })
-})
-
-/*
-  @route    PATCH: /exams/:id/status
-  @access   private
-  @desc     Update exam status
-*/
-const updateExamStatus = asyncHandler(async (req, res, next) => {
-  const id = Number(req.params.id)
-
-  const { status } = await examStatusUpdateValidator.validate(req.body, {
+  const data = await classRoutineValidator(id).validate(req.body, {
     abortEarly: false,
   })
 
   await prisma.$transaction(async (tx) => {
-    const findExam = await tx.exams.findUnique({
+    const classRoutine = await tx.class_routines.findUnique({
       where: {
         id,
       },
     })
 
-    if (!findExam)
+    if (!classRoutine)
       return res.status(404).json({
-        message: 'No exam found',
+        message: 'No class routine found',
       })
 
-    if (findExam.status === 'ACTIVE') {
-      return res.status(400).json({
-        message: 'Cannot change status for ongoing exam',
-      })
-    }
-
-    await tx.exams.update({
+    await prisma.class_routines.update({
       where: {
-        id: findExam.id,
+        id,
       },
-      data: {
-        status,
-      },
+      data,
     })
 
-    res.json({
-      message: 'Status updated',
-    })
+    res.json({ message: 'Class routine updated' })
   })
 })
 
 /*
-  @route    DELETE: /exams/:id
+  @route    DELETE: /class-routines/:id
   @access   private
-  @desc     delete an exam
+  @desc     delete class routine
 */
-const deleteExam = asyncHandler(async (req, res, next) => {
+const deleteClassRoutine = asyncHandler(async (req, res, next) => {
   const id = Number(req.params.id)
 
   await prisma.$transaction(async (tx) => {
-    const findExam = await tx.exams.findUnique({
+    const classRoutine = await tx.class_routines.findUnique({
       where: {
         id,
       },
     })
 
-    if (!findExam)
+    if (!classRoutine)
       return res.status(404).json({
-        message: 'No exam found',
+        message: 'No class routine found',
       })
 
-    await tx.exams.delete({
+    await tx.class_routines.delete({
       where: { id },
     })
 
-    res.json({ message: 'Exam deleted successfully' })
+    res.json({ message: 'Class routine deleted' })
   })
 })
 
 module.exports = {
   getAllClassRoutines,
+  getClassRoutine,
+  createClassRoutine,
+  updateClassRoutine,
+  deleteClassRoutine,
 }
--- routes/classRoutineRoutes.js
+++ routes/classRoutineRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middlewares/authMiddleware')
-const { getAllClassRoutines } = require('../controllers/classRoutineController')
+const {
+  getAllClassRoutines,
+  getClassRoutine,
+  createClassRoutine,
+  updateClassRoutine,
+  deleteClassRoutine,
+} = require('../controllers/classRoutineController')
 
 // Protected Routes
 router.get('/', authMiddleware(), getAllClassRoutines)
+router.get('/:id', authMiddleware(), getClassRoutine)
+router.post('/', authMiddleware(), createClassRoutine)
+router.put('/:id', authMiddleware(), updateClassRoutine)
+router.delete('/:id', authMiddleware(), deleteClassRoutine)
 
 module.exports = router
