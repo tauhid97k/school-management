a1530ba0690925a7c54b4e5022326a0a72061e38
Onirudda Islam <113366220+onirudda-islam@users.noreply.github.com>
2024-03-06 16:45:19 +0600

    Update app.js

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use(
 )
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
