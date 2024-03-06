0573ab1f176b32d3749bacc9573947c10b56f7dc
Onirudda Islam <113366220+onirudda-islam@users.noreply.github.com>
2024-03-06 16:54:57 +0600

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.use(
   })
 )
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Cross-Origin-Resource-Policy', 'cross-origin')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
