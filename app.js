0a1174bf9c8fee2e2e00982938335ccc753c24ff
Onirudda Islam <113366220+onirudda-islam@users.noreply.github.com>
2024-03-06 15:57:34 +0600

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ const limiter = rateLimit({
 app.use(
   cors({
     origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 )
