53f38ad68cb7b2b793f4484f2863010e97b40e7e
tauhid97k <tauhid.coder@gmail.com>
2024-03-06 19:26:16 +0600

    cors update.

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ const limiter = rateLimit({
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:3000',
-      'http://192.168.1.170:5173',
-      'http://192.168.1.170:5174',
+      'https://schoolapi.silicornya.net',
+      'https://school.silicornya.net',
+      'https://schoolstaff.silicornya.net',
+      'http://127.0.0.1:5000',
+      '*',
     ],
     credentials: true,
   })
