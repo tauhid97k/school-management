5613db7d345df9c2d4a59f97b5fe8f96928977c9
tauhid97k <tauhid.coder@gmail.com>
2024-05-18 12:03:47 +0600

    added new endpoint in cors

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(
       'http://27.147.163.201:5174',
       'http://localhost:5173',
       'http://localhost:5174',
+      'http://localhost:3000',
       'http://127.0.0.1:5000',
       '*',
     ],
